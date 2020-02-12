526f341fef6a6877509ae3f67ed117451b0741b9
Dariusz Franczak <darius32@op.pl>
2020-02-12 21:49:28 +0000

    completed

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -3,17 +3,12 @@ import './ListItem.css';
 
 import danAbramovImage from '../../../assets/images/danabramov.jpg';
 
-const ListItem = () => (
+const ListItem = (props) => (
   <li className="listItem__wrapper">
-    <img className="listItem__image" src={danAbramovImage} />
+    <img className="listItem__image" src={props.image} />
     <div>
-      <h2 className="listItem__name">Dan Abramov</h2>
-      <p className="listItem__description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptate minus at soluta esse voluptatum quam eligendi dolore,
-        ipsa explicabo, alias in numquam tempore totam inventore vero
-        perspiciatis recusandae id nulla.
-      </p>
+      <h2 className="listItem__name">{props.name}</h2>
+      <p className="listItem__description">{props.description}</p>
       <button className="listItem__button">Visit Twitter page</button>
     </div>
   </li>
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './ListWrapper.css';
 
 import ListItem from './ListItem/ListItem';
+import { twitterAccounts } from '../../data/twitterAccounts';
+
 
 const ListWrapper = () => (
   <ul className="listWrapper__wrapper">
-    <ListItem />
+    <ListItem
+      image={twitterAccounts[0].image}
+      name={twitterAccounts[0].name}
+      description={twitterAccounts[0].description}
+    />
   </ul>
 );
 

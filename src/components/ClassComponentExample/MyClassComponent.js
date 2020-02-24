7d9e13d19e9e03e9060301a546afbefcfc463083
Dariusz Franczak <darius32@op.pl>
2020-02-24 11:35:13 +0000

    completed

--- src/components/ClassComponentExample/MyClassComponent.js
+++ src/components/ClassComponentExample/MyClassComponent.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class MyClassComponent extends React.Component {
+  state = {
+    text: 'This text comes from state.',
+    inputText: ''
+  }
+
+  onChangeHandler = (event) => console.log(event.target.value);
+
   render() {
     return (
-      <h1>The basic class component</h1>
+      <>
+        <h1>The basic class component</h1>
+        <h2>{this.state.text}</h2>
+        <input
+          type="text"
+          placeholder="Enter your text"
+          onChange={this.onChangeHandler}
+        />
+        <h2>{this.state.inputText}</h2>
+      </>
     );
   }
 }

6d84e5d920d2a72408465a0a2fb67bdc24980780
Dariusz Franczak <darius32@op.pl>
2020-02-24 11:19:00 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 import ListWrapper from './components/ListWrapper/ListWrapper';
+import MyClassComponent from './components/ClassComponentExample/MyClassComponent';
 
 function App() {
   return (
     <div className="App">
+      <MyClassComponent />
       <ListWrapper />
     </div>
   );
---
+++ src/components/ClassComponentExample/MyClassComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyClassComponent extends React.Component {
+  render() {
+    return (
+      <h1>The basic class component</h1>
+    );
+  }
+}
+
+export default MyClassComponent;
